99cc34a8a8a10d3bf4ad5b5b4e62c9d0e7a2b8f8
OmarKhaled <omarabdelaal2013@gmail.com>
2024-03-07 22:31:14 +0200

    Add NoteItem and Custom Font

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,66 @@
+
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        top: 24,
+        bottom: 24,
+        left: 16,
+      ),
+      decoration: BoxDecoration(
+        color: const Color(0xffffcc80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Padding(
+              padding: EdgeInsets.only(bottom: 16),
+              child: Text(
+                'Flutter Tips',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Text(
+                'Build your career with Omar Khaled',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.black,
+                size: 30,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              'May21 , 2022',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.5),
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
