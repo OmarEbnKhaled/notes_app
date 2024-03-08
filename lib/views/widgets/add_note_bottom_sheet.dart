273fb5555b8745bbb265c856f8cc5a63d8281c52
OmarKhaled <omarabdelaal2013@gmail.com>
2024-03-08 02:26:42 +0200

    Create CustomTextFlied and ModelBottomSheet

---
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(height: 32),
+          CustomTextField(),
+        ],
+      ),
+    );
+  }
+}
